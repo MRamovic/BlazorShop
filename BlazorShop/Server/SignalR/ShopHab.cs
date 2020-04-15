b6b2576376c0d4e5b9240713ff8e3c6d20cfca7c
MIrza Ramovic <mirza.ramovic@live.com>
2020-04-15 20:52:11 +0200

    BlazorShop

--- BlazorShop/Server/SignalR/ShopHab.cs
+++ BlazorShop/Server/SignalR/ShopHab.cs
@@ -10,11 +10,15 @@ namespace BlazorShop.Server.SignalR
 {
     public class ShopHab : Hub
     {
-
-       
-
-
-
+        public async Task SviArtikli()
+        {
+            Console.WriteLine("U Metodi SviArtikli Hab!");
+            Baza NB = new Baza();
+            List<Artikal> lArt2 = new List<Artikal>();
+            lArt2 = NB.Artikals.ToList();
+            Console.WriteLine("Prosla metoda svi artikli Hab!");
+            await Clients.Caller.SendAsync("ArtikalMetoda", lArt2);              
+        }
      
     }
 }
